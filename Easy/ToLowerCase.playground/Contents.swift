a4e5be9dec2c9d45c574d89348f25f09094acc60
Artem Listopadov <artbdarm9@mail.ru>
2022-11-18 21:39:33 +0300

    feat: add solutions
    Task name - To Lower Case

--- Easy/ToLowerCase.playground/Contents.swift
+++ Easy/ToLowerCase.playground/Contents.swift
@@ -1,29 +1,47 @@
 
 /*
  Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
-
+ 
  Example 1:
-
+ 
  Input: s = "Hello"
  Output: "hello"
  Example 2:
-
+ 
  Input: s = "here"
  Output: "here"
  Example 3:
-
+ 
  Input: s = "LOVELY"
  Output: "lovely"
-
+ 
  */
 
 class Solution {
-    func toLowerCase(_ s: String) -> String {
+    // First solution
+    func toLowerCaseFirst(_ s: String) -> String {
         s.lowercased()
+    }
+    
+    // Second solution
+    func toLowerCaseSecond(_ s: String) -> String {
+        var result = ""
+        for i in s {
+            if let char = i.asciiValue, "A" <= i && i <= "Z" {
+                result.append(Character(UnicodeScalar(char + 32)))
+            } else {
+                result.append(i)
+            }
+        }
+        return result
     }
 }
 
 let solution = Solution()
-solution.toLowerCase("Hello")   // "hello"
-solution.toLowerCase("here")   // "hello"
-solution.toLowerCase("LOVELY")   // "lovely"
+solution.toLowerCaseFirst("Hello")   // "hello"
+solution.toLowerCaseFirst("here")   // "hello"
+solution.toLowerCaseFirst("LOVELY")   // "lovely"
+
+solution.toLowerCaseSecond("Hello")   // "hello"
+solution.toLowerCaseSecond("here")   // "hello"
+solution.toLowerCaseSecond("LOVELY")   // "lovely"
